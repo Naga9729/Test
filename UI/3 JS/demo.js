5e0ee5b2a42c8980fd018c74c6c88769ac44db23
ctsjava <ctsgit@gmail.com>
2019-03-01 12:08:56 +0530

    Java Script

---
+++ UI/3 JS/demo.js
@@ -0,0 +1,49 @@
+console.log('--- data types')
+
+// 1. Undefined
+
+var x;
+
+// 2. Strings
+var s1='Abc';
+var s2="Xyz";
+// Cancat
+
+var s3=s1+s2;
+
+var name="Praveen";
+
+var s4="Hello "+name;
+
+// EL
+var s5=`Hello dear ${name}`; 
+
+// 3. Numbers
+
+var x;
+x=10+20;
+
+// 4. boolean
+
+var x=false;
+
+// Objects
+// How to crate Object in JS
+
+var emp={
+    id:1024,
+    name:'Praveen',
+    salary:34634
+}
+
+
+
+var p=new Object();
+
+var any={};
+
+
+
+
+
+
