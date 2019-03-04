25c4bf18843ce3ad3cd7c6868d5045f50eb9b110
ctsjava <ctsgit@gmail.com>
2019-03-04 10:36:44 +0530

    Java Script

--- UI/3 JS/demo.js
+++ UI/3 JS/demo.js
@@ -1,12 +1,25 @@
-// ES -6
-// let
 
-let x=10;
-console.log('X is '+x);
-
-let i=10;
-//let i=20;
+//  javascript closures
 
 
+let a,b,c;
+a=100;
+c=a;
+a=200;
+
+function doTeach(sub,trainer){
+    console.log('-- teaching '+sub+' by '+trainer);
+   abc= function(task){
+        console.log('doing '+task+' task given by '+trainer);  
+    }
+    return abc;
+    //doTask('Collections API')
+}
+var t1=doTeach('java','praveen');
+t1('Threads')
+t1('COlections');
+
+var t2=doTeach('java script','james');
+t2('data types');
 
 
