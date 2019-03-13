2a2c88d956c071913a722fb1a27e7cea98e65ab5
ctsjava <ctsgit@gmail.com>
2019-03-13 13:10:16 +0530

    Spring Boot added

--- Spring/SpringBoot-MVC/src/main/java/com/cts/product/MyController.java
+++ Spring/SpringBoot-MVC/src/main/java/com/cts/product/MyController.java
@@ -3,6 +3,7 @@ package com.cts.product;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class MyController {
@@ -11,35 +12,36 @@ public class MyController {
 	public void f1() {
 		System.out.println("------ MyController f1 method");
 	}
-	
+
 	@RequestMapping("/s2")
 	public void f2() {
 		System.out.println("--- Thsi is my f2 method");
 	}
-	
+
 	@RequestMapping("/s3")
 	public String f3() {
 		System.out.println("--- This is f3 method");
 		return "one";
 	}
-	
-	
-	
+
 	@RequestMapping("/s4")
 	public String f4(Model model) {
 		System.out.println("--- This is f4 method");
 		// DB code
-		String userName="Praveen Reddy S";
-		model.addAttribute("uname",userName);
+		String userName = "Praveen Reddy S";
+		model.addAttribute("uname", userName);
 		return "two";
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
+
+	@RequestMapping("/s5")
+	public ModelAndView f5() {
+		System.out.println("--- This is f5 method");
+		String userName = "Ozvitha";
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("thr");
+		mav.addObject("uname", userName);
+
+		return mav;
+	}
+
 }
